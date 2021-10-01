d63abd628212ee81f019b35b319bf3374320a096
israel <israel.pereira@dcx.ufpb.br>
2021-10-01 19:38:44 -0300

    contador

---
+++ Contador/script.js
@@ -0,0 +1,35 @@
+function contar() {
+  let inicio = document.getElementById('txti')
+  let fim = document.getElementById('txtf')
+  let passo = document.getElementById('txtp')
+  let res = document.getElementById('res')
+
+  if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+    res.innerHTML = 'Impossível contar'
+    // window.alert('[ERRO] Faltam dados!')
+  } else {
+    res.innerHTML = 'Contando: <br>'
+    let i = Number(inicio.value)
+    let f = Number(fim.value)
+    let p = Number(passo.value)
+
+    if (p <= 0) {
+      window.alert('Passo inválido! Considerando PASS 1')
+      p = 1
+    }
+    
+    if (i < f) {
+       // contagem crescente
+      for(let c = i; c <= f; c += p) {
+        res.innerHTML += `${c} \u{1F449}`
+       }
+    } else {
+       // contagem regressiva
+      for(let c = i; c >= f; c -= p) {
+        res.innerHTML += `${c} \u{1F449}`
+       }
+    }
+    res.innerHTML += `\u{1F3C1}`
+  }
+  
+}
