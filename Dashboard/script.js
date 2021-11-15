6d1cda1c0118430fa7fd8a0252aa73b853a4ec36
israel <israel.pereira@dcx.ufpb.br>
2021-11-15 13:02:30 -0300

    modificações files

--- Dashboard/script.js
+++ Dashboard/script.js
@@ -1,1 +1,10 @@
-let list = document.querySelectorAll('.navigation li')
+// adicionar classe pairada no item da lista selecionada
+let list = document.querySelectorAll('.navigation li');
+
+function activeLink() {
+  list.forEach((item) => 
+    item.classList.remove('hovered'));
+    this.classList.add('hovered');
+}
+list.forEach((item) =>
+item.addEventListener('mouseover', activeLink));
